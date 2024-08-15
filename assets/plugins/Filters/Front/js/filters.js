93de313e38dbb8dcbbc115ca73e197f4d884a563
zif91 <65787947+zif91@users.noreply.github.com>
2024-08-15 14:46:05 +0300

    Update filters.js

--- assets/plugins/Filters/Front/js/filters.js
+++ assets/plugins/Filters/Front/js/filters.js
@@ -1,33 +1,38 @@
-function initFiltersListeners()
-{
-  document
-    .querySelector('[data-filters-button]')
-    .addEventListener('click', function (event) {
-      const url = '/filters/'
-      const button = event.target
-      const form = button.closest('form')
-      const formData = $(form).serializeArray()
-      let newUrl = ''
+function initFiltersListeners() {
+  const filtersButton = document.querySelector('[data-filters-button]');
+
+  // Проверяем, существует ли элемент перед добавлением обработчика события
+  if (filtersButton) {
+    filtersButton.addEventListener('click', function (event) {
+      const url = '/filters/';
+      const button = event.target;
+      const form = button.closest('form');
+      const formData = $(form).serializeArray();
+      let newUrl = '';
 
       formData.forEach(function (item) {
         if (item.value.length > 0) {
-          item.value = item.value.replace(/ /g, '+')
+          item.value = item.value.replace(/ /g, '+');
 
           if (item.value.includes(':')) {
-            item.value = 'range-' + item.name.replace(/[^+\d]/g, '') + '-' + item.value.replace(':', '-')
+            item.value = 'range-' + item.name.replace(/[^+\d]/g, '') + '-' + item.value.replace(':', '-');
           }
 
-          newUrl = `${newUrl}${encodeURI(item.value)}/`
+          newUrl = `${newUrl}${encodeURI(item.value)}/`;
         }
-      })
+      });
 
       if (newUrl.length > 0) {
-        newUrl = `${url}${newUrl}`
-        $(form).attr('action', newUrl)
+        newUrl = `${url}${newUrl}`;
+        $(form).attr('action', newUrl);
       }
 
-      form.submit()
-    })
+      form.submit();
+    });
+  } else {
+    console.warn('Element with [data-filters-button] not found.');
+  }
 }
 
-initFiltersListeners();
+// Запускаем функцию только после полной загрузки DOM
+document.addEventListener('DOMContentLoaded', initFiltersListeners);
